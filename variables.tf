b9eba99b080aaad5cfab73a1f101e4d596711494
elitesolutionit <elitesolutionsit@outlook.com>
2022-08-14 13:20:33 -0500

    adding more description

--- variables.tf
+++ variables.tf
@@ -1,11 +1,6 @@
 variable "subcription_name" {
   type        = string
   description = "Main Subscription for deployments"
-
-  validation {
-    condition     = var.subcription_name == "EliteSolutionsIT-DEV"
-    error_message = "Please choose a valid subscription."
-  }
 }
 
 variable "env" {
@@ -20,30 +15,37 @@ variable "env" {
 
 variable "agent_name" {
   type = string
+  description = "The name which should be used for the default Kubernetes Node Pool. Changing this forces a new resource to be created"
 }
 
-variable "agent_count" {
+variable "node_count" {
+  description = "The initial number of nodes which should exist in this Node Pool. If specified this must be between 1 and 1000 and between min_count and max_count."
   type = string
 }
 
 variable "vm_size" {
   type = string
+  description = "The size of the Virtual Machine, such as Standard_DS2_v2. Changing this forces a new resource to be created."
 }
 
 variable "admin_username" {
+  description = "The Admin Username for the Cluster. Changing this forces a new resource to be created."
   type = string
 }
 
 variable "key_data" {
+  description = "The Public SSH Key used to access the cluster. Changing this forces a new resource to be created."
   type = string
 }
 
 variable "resource_group_name" {
   type = string
+  description = "Specifies the Resource Group where the Managed Kubernetes Cluster should exist. Changing this forces a new resource to be created."
 }
 
 
 variable "service_principal" {
+  description = "block supports the following:"
   type = list(object({
     client_id     = string
     client_secret = string
@@ -51,6 +53,7 @@ variable "service_principal" {
 }
 
 variable "network_profile" {
+  description = "block supports the following:"
   type = list(object({
     load_balancer_sku = string
     network_plugin    = string
@@ -58,13 +61,7 @@ variable "network_profile" {
 }
 
 variable "k8s" {
+  description = "Managed Kubernetes Cluster to create. Changing this forces a new resource to be created."
   type        = map(any)
-  description = "Kubernetes cluster to be deployed"
-
-  default = {}
-}
-
-variable "cluster_node_pool" {
-  type    = map(any)
   default = {}
 }
